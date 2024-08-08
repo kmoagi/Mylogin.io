0a2d414853c0e54089982b42a2cc235fe9008232
Kabelo <33934323+kmoagi@users.noreply.github.com>
2024-08-08 14:54:44 +0200

    qoom sync at Thu Aug 08 2024 12:54:44 GMT+0000 (Coordinated Universal Time)

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ f
 window.logout = function() {
 
 	// TELLS OUR SERVER TO LOG THE USER OUT
-	fetch('/~/Doggo/logout', { method: 'POST'});
+	 fetch('/~/logindemo/logout', { method: 'POST'});
 	
 	// TELLS OUR BROWSER TO SHOW A MESSAGE TO OUR USER
 	alert('Logged Out!');
 	
 	// REDIRECTS THE USER TO THE HOMEPAGE
-	location.href = '/~/Doggo/index'
+	location.href = '/~/logindemo/index'
 }
